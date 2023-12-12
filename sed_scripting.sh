d4d8be45346c27ae494d030ffb0aabe9689789cb
oiram <90894360+oiram-it@users.noreply.github.com>
2023-12-12 15:27:55 +0100

    Delete sed_scripting.sh

--- sed_scripting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in `cat namefile.txt`
-do
-        sed -i.bak 's|search_for/with_slash|replace_string|g' $file
-done
-
-###sed -i 's/search_for/replace_with/g' /path/filename
